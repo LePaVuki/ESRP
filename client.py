59006f81862e2c0278d6dcbdb3b9a0cd60853a96
LePaVuki <pavuki2000@gmail.com>
2024-04-04 22:54:23 +0500

    Realised route building using API calls and output of the route system names in a list

---
+++ client.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+BASE_URL = "https://esi.evetech.net/latest"
+
+ROUTE_ORIGIN = "30000142"  # Jita
+ROUTE_DESTINATION = "30000165"  # Ishisomo
+
+# Build a route between ROUTE_ORIGIN and ROUTE_DESTINATION
+# Get route data based on a list of ids
+route_ids = requests.get(
+    f"{BASE_URL}/route/{ROUTE_ORIGIN}/{ROUTE_DESTINATION}/?datasource=tranquility"
+)
+
+# Route data is in str
+route_data_str = requests.post(f"{BASE_URL}/universe/names", route_ids).text
+
+# Transform raw str to JSON object
+route_data = json.loads(route_data_str)
+# Select only names of route systems in JSON object
+route_names = [x["name"] for x in route_data]
+
+print(route_names)
